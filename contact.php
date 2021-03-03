3104fa7cbe789de2d78d582e26f8482190ea80d6
Ashton Smith <smithast@iu.edu>
2021-03-03 13:22:12 -0500

    commit

--- contact.php
+++ contact.php
@@ -1,11 +1,14 @@
 <?php
 
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
+// If necessary, modify the path in the require statement below to refer to the
+// location of your Composer autoload.php file.
 require 'vendor/autoload.php';
+
 if(!$_POST) exit;
 
 // Email address verification, do not edit.
@@ -45,72 +48,80 @@ if(get_magic_quotes_gpc()) {
 	$comments = stripslashes($comments);
 }
 
-
-// Configuration option.
-// Enter the email address that you want to emails to be sent to.
-// Example $address = "joe.doe@yourdomain.com";
-
-//$address = "example@themeforest.net";
-$address = "ashton@newtrostudios.com";
-
-
-// Configuration option.
-// i.e. The standard subject will appear as, "You've been contacted by John Doe."
-
-// Example, $e_subject = '$name . ' has contacted you via Your Website.';
-
 $e_subject = 'You\'ve been contacted by ' . $first_name . '.';
-
-
-// Configuration option.
-// You can change this if you feel that you need to.
-// Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
-
 $e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
 $e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
 $e_reply = "You can contact $first_name via email, $email or via phone $phone";
 
 $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+// Replace sender@example.com with your "From" address.
+// This address must be verified with Amazon SES.
+$sender = 'ashton@newtrostudios.com';
+$senderName = 'Louisville Cabinets';
 
-$headers = "From: $email" . PHP_EOL;
-$headers .= "Reply-To: $email" . PHP_EOL;
-$headers .= "MIME-Version: 1.0" . PHP_EOL;
-$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
-$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+// Replace recipient@example.com with a "To" address. If your account
+// is still in the sandbox, this address must be verified.
+$recipient = 'ashton@newtrostudios.com';
+
+// Replace smtp_username with your Amazon SES SMTP user name.
+$usernameSmtp = 'AKIARRXL7UGNQW6QYCVL';
+
+// Replace smtp_password with your Amazon SES SMTP password.
+$passwordSmtp = 'BPqyx9OClh3bdnwh0cw7lESR8Rmnm2UWD2AuayxjGugh';
+
+// Specify a configuration set. If you do not want to use a configuration
+// set, comment or remove the next line.
+// $configurationSet = 'ConfigSet';
+
+// If you're using Amazon SES in a region other than US West (Oregon),
+// replace email-smtp.us-west-2.amazonaws.com with the Amazon SES SMTP
+// endpoint in the appropriate region.
+$host = 'email-smtp.us-east-2.amazonaws.com';
+$port = 587;
+
+// // The subject line of the email
+// $subject = 'Amazon SES test (SMTP interface accessed using PHP)';
+
+// // The plain-text body of the email
+// $bodyText =  "Email Test\r\nThis email was sent through the
+//     Amazon SES SMTP interface using the PHPMailer class.";
+
+// // The HTML-formatted body of the email
+// $bodyHtml = '<h1>Email Test</h1>
+//     <p>This email was sent through the
+//     <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
+//     interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+//     PHPMailer</a> class.</p>';
 
 $mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'email-smtp.us-east-2.amazonaws.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'AKIARRXL7UGNQW6QYCVL';                     //SMTP username
-    $mail->Password   = 'BPqyx9OClh3bdnwh0cw7lESR8Rmnm2UWD2AuayxjGugh';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 25;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    // Specify the SMTP settings.
+    $mail->isSMTP();
+    $mail->setFrom($sender, $senderName);
+    $mail->Username   = $usernameSmtp;
+    $mail->Password   = $passwordSmtp;
+    $mail->Host       = $host;
+    $mail->Port       = $port;
+    $mail->SMTPAuth   = true;
+    $mail->SMTPSecure = 'tls';
+    // $mail->addCustomHeader('X-SES-CONFIGURATION-SET', $configurationSet);
 
-    //Recipients
-    $mail->setFrom($address, 'Mailer');
-    // $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    $mail->addAddress('ashton@newtrostudios.com');               //Name is optional
-    // $mail->addReplyTo('info@example.com', 'Information');
-    // $mail->addCC('cc@example.com');
-    // $mail->addBCC('bcc@example.com');
+    // Specify the message recipients.
+    $mail->addAddress($recipient);
+    // You can also add CC, BCC, and additional To recipients here.
 
-    //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $e_subject;
-    $mail->Body    = $e_body;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Message has been sent';
+    // Specify the content of the message.
+    $mail->isHTML(true);
+    $mail->Subject    = $e_subject;
+    $mail->Body       = $msg;
+    $mail->AltBody    = $e_body;
+    $mail->Send();
+    echo "Email sent!" , PHP_EOL;
+} catch (phpmailerException $e) {
+    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
 }
+
+?>
