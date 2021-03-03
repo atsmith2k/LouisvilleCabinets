7d87cc0f71cfbd8b3162a87840f6fe67cbce6953
Ashton Smith <smithast@iu.edu>
2021-03-03 13:24:44 -0500

    commit

--- contact.php
+++ contact.php
@@ -13,10 +13,10 @@ if(!$_POST) exit;
 
 // Email address verification, do not edit.
 function isEmail($email) {
-	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
+	return(preg_match('/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i',$email));
 }
 
-if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+if (!defined('PHP_EOL')) define('PHP_EOL', '\r\n');
 
 $first_name     = $_POST['first_name'];
 $last_name     = $_POST['last_name'];
@@ -29,18 +29,18 @@ $comments = $_POST['comments'];
 $verify   = $_POST['verify'];
 
 if(trim($first_name) == '') {
-	echo '<div class="error_message">Attention! You must enter your name.</div>';
+	echo '<div class='error_message'>Attention! You must enter your name.</div>';
 	exit();
 }  else if(trim($email) == '') {
-	echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
+	echo '<div class='error_message'>Attention! Please enter a valid email address.</div>';
 	exit();
 } else if(!isEmail($email)) {
-	echo '<div class="error_message">Attention! You have enter an invalid e-mail address, try again.</div>';
+	echo '<div class='error_message'>Attention! You have enter an invalid e-mail address, try again.</div>';
 	exit();
 }
 
 if(trim($comments) == '') {
-	echo '<div class="error_message">Attention! Please enter your message.</div>';
+	echo '<div class='error_message'>Attention! Please enter your message.</div>';
 	exit();
 }
 
@@ -49,17 +49,17 @@ if(get_magic_quotes_gpc()) {
 }
 
 $e_subject = 'You\'ve been contacted by ' . $first_name . '.';
-$e_body = "You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project." . PHP_EOL . PHP_EOL;
-$e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
-$e_reply = "You can contact $first_name via email, $email or via phone $phone";
+$e_body = 'You have been contacted by $first_name. $first_name selected service of $select_service, their additional message is as follows. Customer max budge is $select_price, for this project.' . PHP_EOL . PHP_EOL;
+$e_content = '\'$comments\'' . PHP_EOL . PHP_EOL;
+$e_reply = 'You can contact $first_name via email, $email or via phone $phone';
 
 $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
-// Replace sender@example.com with your "From" address.
+// Replace sender@example.com with your 'From' address.
 // This address must be verified with Amazon SES.
 $sender = 'ashton@newtrostudios.com';
 $senderName = 'Louisville Cabinets';
 
-// Replace recipient@example.com with a "To" address. If your account
+// Replace recipient@example.com with a 'To' address. If your account
 // is still in the sandbox, this address must be verified.
 $recipient = 'ashton@newtrostudios.com';
 
@@ -83,14 +83,14 @@ $port = 587;
 // $subject = 'Amazon SES test (SMTP interface accessed using PHP)';
 
 // // The plain-text body of the email
-// $bodyText =  "Email Test\r\nThis email was sent through the
-//     Amazon SES SMTP interface using the PHPMailer class.";
+// $bodyText =  'Email Test\r\nThis email was sent through the
+//     Amazon SES SMTP interface using the PHPMailer class.';
 
 // // The HTML-formatted body of the email
 // $bodyHtml = '<h1>Email Test</h1>
 //     <p>This email was sent through the
-//     <a href="https://aws.amazon.com/ses">Amazon SES</a> SMTP
-//     interface using the <a href="https://github.com/PHPMailer/PHPMailer">
+//     <a href='https://aws.amazon.com/ses'>Amazon SES</a> SMTP
+//     interface using the <a href='https://github.com/PHPMailer/PHPMailer'>
 //     PHPMailer</a> class.</p>';
 
 $mail = new PHPMailer(true);
@@ -117,11 +117,11 @@ try {
     $mail->Body       = $msg;
     $mail->AltBody    = $e_body;
     $mail->Send();
-    echo "Email sent!" , PHP_EOL;
+    echo 'Email sent!' , PHP_EOL;
 } catch (phpmailerException $e) {
-    echo "An error occurred. {$e->errorMessage()}", PHP_EOL; //Catch errors from PHPMailer.
+    echo 'An error occurred. {$e->errorMessage()}', PHP_EOL; //Catch errors from PHPMailer.
 } catch (Exception $e) {
-    echo "Email not sent. {$mail->ErrorInfo}", PHP_EOL; //Catch errors from Amazon SES.
+    echo 'Email not sent. {$mail->ErrorInfo}', PHP_EOL; //Catch errors from Amazon SES.
 }
 
 ?>
