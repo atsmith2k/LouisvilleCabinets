0a3347242d254b7042d63217cf69f8c846cd75a2
Ashton Smith <smithast@iu.edu>
2021-03-02 22:28:14 -0500

    commit

--- contact.php
+++ contact.php
@@ -72,7 +72,7 @@ $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
 
-if(mail($address, $e_subject, $msg, $headers)) {
+if(imap_mail($address, $e_subject, $msg)) {
 
 	// Email has sent successfully, echo a success page.
 
